4113e4f6a3aaa052aac7556feba12297481904d8
OmarAbdElsalam1998 <O.Abdulsalam16.121@compit.au.edu.eg>
2023-06-27 18:54:56 +0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,30 +78,30 @@ var $owlCarousel =$('.owl-carousel.two').owlCarousel({
   //     }
   //   }
   // });
-  var ctx = document.getElementById("canvas1").getContext("2d");
-  var myChart = new Chart(ctx, {
-      type: 'bar',
+  // var ctx = document.getElementById("canvas1").getContext("2d");
+  // var myChart = new Chart(ctx, {
+  //     type: 'bar',
       
-      // The data for our dataset
-      data: {
-          labels:["8","6","4","10","4","10"],
-          datasets: [{
-              label: 'My First dataset',
-              backgroundColor: ["#F57F17", "#F9A825", "#FBC02D" ,"#FDD835","#FFEB3B","#FFEE58", "#FFF176", "#FFF59D" ,"#FFF9C4","#FFFDE7","#FFE082", "#FFD54F", "#FFCA28" ,"#FFC107","#FFB300"],
-              data: [8,6,4,10,4,10]
-          }]
-      },
+  //     // The data for our dataset
+  //     data: {
+  //         labels:["8","6","4","10","4","10"],
+  //         datasets: [{
+  //             label: 'My First dataset',
+  //             backgroundColor: ["#F57F17", "#F9A825", "#FBC02D" ,"#FDD835","#FFEB3B","#FFEE58", "#FFF176", "#FFF59D" ,"#FFF9C4","#FFFDE7","#FFE082", "#FFD54F", "#FFCA28" ,"#FFC107","#FFB300"],
+  //             data: [8,6,4,10,4,10]
+  //         }]
+  //     },
       
-      // Configuration options go here
-      options: {
+  //     // Configuration options go here
+  //     options: {
            
-          display:true,
-          text:'Time Used In A Day Chart (Shown in %)',
-          responsive: true,
-          maintainAspectRatio:false,
-          showScale: false
-      }
-  });
+  //         display:true,
+  //         text:'Time Used In A Day Chart (Shown in %)',
+  //         responsive: true,
+  //         maintainAspectRatio:false,
+  //         showScale: false
+  //     }
+  // });
   var barChartData = {
     labels : ["8","6","4","10","4","10"],
     datasets : [
@@ -134,7 +134,7 @@ var $owlCarousel =$('.owl-carousel.two').owlCarousel({
     
   }
 
-var myLine = new Chart(document.getElementById("canvas2").getContext("2d")).Bar(barChartData);
+var myLine = new Chart(document.getElementById("canvas21").getContext("2d")).Bar(barChartData);
 
 
 var lineChartData2 = {
@@ -151,4 +151,58 @@ var lineChartData2 = {
   
 }
 
-var myLine2 = new Chart(document.getElementById("canvas1").getContext("2d")).Line(lineChartData2);
+var myLine2 = new Chart(document.getElementById("canvas11").getContext("2d")).Line(lineChartData2);
+
+
+
+
+var barChartData2 = {
+  labels : ["8","6","4","10","4","10"],
+  datasets : [
+    {
+      fillColor : "#FFBE3F",
+      strokeColor : "rgba(220,220,220,1)",
+      data : [8,6,4,10,4,10]
+    }],
+    options: {
+      display:true,
+      text:'Time Used In A Day Chart (Shown in %)',
+      responsive: true,
+      maintainAspectRatio:false,
+      showScale: false,
+      scales: {
+        x: {
+          grid: {
+            display: false
+          }
+        },
+        y: {
+          grid: {
+            display: false
+          }
+        }
+      }
+  }
+    
+  
+  
+}
+
+var myLine4 = new Chart(document.getElementById("canvas22").getContext("2d")).Bar(barChartData2);
+
+
+var lineChartData1 = {
+labels : ["5th","4th","3th","2th","1th"],
+datasets : [
+  {
+    fillColor : "rgba(220,220,220,0.5)",
+    strokeColor : "#00C8AE",
+    pointColor : "#00C8AE",
+    pointStrokeColor : "#fff",
+    data : [10,8,10,7,2]
+  }
+]
+
+}
+
+var myLine3 = new Chart(document.getElementById("canvas12").getContext("2d")).Line(lineChartData1);
