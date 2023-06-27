f4d1debdbe45a47b59dfb76b0132c9f862a1a9d8
OmarAbdElsalam1998 <O.Abdulsalam16.121@compit.au.edu.eg>
2023-06-27 18:58:20 +0300

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,51 +57,6 @@ var $owlCarousel =$('.owl-carousel.two').owlCarousel({
   
   
   
-
-  // const ctx = document.getElementById('canvas').getContext("2d")).Bar(barChartData);
-
-  // new Chart(ctx, {
-  //   type: 'bar',
-  //   data: {
-  //     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-  //     datasets: [{
-  //       label: '# of Votes',
-  //       data: [12, 19, 3, 5, 2, 3],
-  //       borderWidth: 1
-  //     }]
-  //   },
-  //   options: {
-  //     scales: {
-  //       y: {
-  //         beginAtZero: true
-  //       }
-  //     }
-  //   }
-  // });
-  // var ctx = document.getElementById("canvas1").getContext("2d");
-  // var myChart = new Chart(ctx, {
-  //     type: 'bar',
-      
-  //     // The data for our dataset
-  //     data: {
-  //         labels:["8","6","4","10","4","10"],
-  //         datasets: [{
-  //             label: 'My First dataset',
-  //             backgroundColor: ["#F57F17", "#F9A825", "#FBC02D" ,"#FDD835","#FFEB3B","#FFEE58", "#FFF176", "#FFF59D" ,"#FFF9C4","#FFFDE7","#FFE082", "#FFD54F", "#FFCA28" ,"#FFC107","#FFB300"],
-  //             data: [8,6,4,10,4,10]
-  //         }]
-  //     },
-      
-  //     // Configuration options go here
-  //     options: {
-           
-  //         display:true,
-  //         text:'Time Used In A Day Chart (Shown in %)',
-  //         responsive: true,
-  //         maintainAspectRatio:false,
-  //         showScale: false
-  //     }
-  // });
   var barChartData = {
     labels : ["8","6","4","10","4","10"],
     datasets : [
